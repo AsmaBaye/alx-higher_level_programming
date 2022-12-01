77cacacf485c106836291e2ff3bf238c953f8a99
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-12-01 10:00:26 -0800

    add files

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- Best genre
 -- select genre.name, sum(ratings) from the joined tables
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS "rating" FROM tv_shows
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS "rating" FROM hbtn_0d_tvshows_rate
  JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
  JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
  JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
