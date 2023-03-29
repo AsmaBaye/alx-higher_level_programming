f4261d1b2e65c757877e16a33d58decf694ebc2c
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-03-29 12:33:04 +0300

    add files

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,11 +15,11 @@ class Square:
         """
 
         if type(size) != int:
-           raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-           raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
-        
+       
     def area(self):
         """A method to return area
 
