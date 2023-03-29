d4090b95afb0224db93b363eb37a94ce9f1ac4d2
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-03-29 11:51:57 +0300

    add files

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,9 +11,9 @@ class Square:
         Args:
         size (int): size of int greater than zero_. Defaults to 0.
         """
- 
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")      
+            raise ValueError("size must be >= 0")
         self.__size = size
