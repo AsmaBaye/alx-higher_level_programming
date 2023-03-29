837cc7a208e2b3738c3244d61ae43e7460b0a540
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-03-29 10:46:32 +0300

    add files

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
-"""Module 1-square 
+"""Module 1-square
 
 This module contain a defination for square class with priv attr
 """
- 
- 
+
+
 class Square:
     """A class that define a square"""
-    
+   
     def __init__(self,size):
-        """Intialize a class with a size
-        Args:
-        	size: _the size of a square
+        """Intialize object with size
+
+		Args:
+			size: _set the size of a square
 		"""
         self.__size = size
