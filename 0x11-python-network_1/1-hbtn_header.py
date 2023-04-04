97491dfa572ae538966c5de9c1f33df4f4d3309d
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-04-04 23:01:49 +0300

    add files

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,3 @@ if __name__ == "__main__":
     data = parse.urlencode({"email": argv[2]}).encode()
     with request.urlopen(argv[1], data) as response:
         print(response.read().decode('utf-8'))
-        
